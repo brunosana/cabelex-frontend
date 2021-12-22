d1bffde568f347f7d81c70f801acca2649c928a8
brunosana <brunosa@dcomp.ufs.br>
2021-12-22 03:24:55 -0300

    feat: Create EditEmployee Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import { ThemeProvider } from 'styled-components';
 
-import { CreateEmployee } from './Pages/CreateEmployee';
+import { EditEmployee } from './Pages/EditEmployee';
 
 import { main } from './styles/themes/main';
 import ISubsidiary from './interfaces/Subsidiary';
+import IEmployee from './interfaces/Employee';
 
 const obj = {
   employeeNumber: 8,
@@ -14,11 +15,17 @@ const obj = {
   name: 'Filial de Tobias Barreto'
 } as ISubsidiary;
 
+const emp = {
+  id: '123456',
+  filial: '123',
+  name: 'Bruno'
+} as IEmployee;
+
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={main}>
     <GlobalStyle />
-    <CreateEmployee/>
+    <EditEmployee employee={emp} subsidiary={obj} />
     </ThemeProvider>
   );
 }
