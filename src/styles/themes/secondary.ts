e96007bd46b8c5327d923fe1cedb62472f11b4cc
brunosana <brunosa@dcomp.ufs.br>
2021-12-23 02:19:18 -0300

    deploy

---
+++ src/styles/themes/secondary.ts
@@ -0,0 +1,23 @@
+const main = {
+    colors: {
+        primary: '#E95B30',
+        shape: '#FFF',
+        half_shape: '#F4EDE8',
+        shape_light: 'rgba(255,255,255,0.6)',
+        shape_overlight: 'rgba(255,255,255,0.14)',
+        background: '#321C3D',
+        error: '#C53030'
+    },
+    roundness: {
+        low: 8,
+        medium: 13,
+        high: 25,
+        ultra: 40
+    },
+    fonts: {
+        cursive: 'Caveat',
+        text: 'Comfortaa',
+    }
+}
+
+export { main };
